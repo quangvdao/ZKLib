f4358facf3d314aaf525d00f63ee1be7d97540c8
Quang Dao <qvd@andrew.cmu.edu>
2024-06-07 15:38:42 -0400

    init BinaryTowerField (not sure how to define it)

---
+++ Jolt/Data/Field/BinaryTowerField.lean
@@ -0,0 +1,10 @@
+import Mathlib.FieldTheory.Tower
+import Mathlib.Data.Polynomial.Basic
+
+/-!
+# Binary Tower Field
+
+Define the binary tower field GF(2^{2^k}) as an iterated quadratic extension of GF(2)
+
+Goal: prove an isomorphism between the definition here and the one in Mathlib
+-/
