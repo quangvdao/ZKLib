766e9734672315c6b50d697edfd61e9f0211b5e7
Quang Dao <qvd@andrew.cmu.edu>
2024-06-18 17:58:42 -0400

    defined AbstractBinaryTower; TODO: prove it's a field

--- Tutorials/MWE.lean
+++ Tutorials/MWE.lean
@@ -1,28 +1,35 @@
 -- Minimum working example (MWE) to post on Zulip
 
 import Mathlib.FieldTheory.Finite.GaloisField
-import Mathlib.Tactic
+
+def otherNat : Type := Nat
 
 
 
--- set_option trace.Meta.synthInstance true in
--- def IteratedExtension (n : ℕ) (F : Type) [CommRing F] : Type :=
---   match n with
---   | 0 => F
---   | n + 1 =>
---   have : CommRing (IteratedExtension n F) := inferInstance
---   AdjoinRoot (Polynomial.X^2 + 1 : Polynomial (IteratedExtension n F))
+noncomputable section
 
--- noncomputable section
+open Polynomial
 
--- def IteratedPolynomial (n : ℕ) : (F : Type) × (Semiring F) :=
---   match n with
---   | 0 => ⟨ ℕ, inferInstance ⟩
---   | n + 1 => ⟨ @Polynomial (IteratedPolynomial n).1 (IteratedPolynomial n).2, inferInstance ⟩
+def IteratedPolynomial (n : ℕ) : (F : Type _) × (CommSemiring F) × List F :=
+  match n with
+  | 0 => ⟨ ℕ, inferInstance, [(1 : ℕ)] ⟩
+  | n + 1 =>
+    let ⟨ Fn, _, elts ⟩ := IteratedPolynomial n
+    ⟨ Polynomial Fn, inferInstance, [(X : Polynomial Fn)] ++ elts.map (fun x => C x) ⟩
 
--- def iterZero : Type _ := (IteratedPolynomial 0).1
+def iterZero : Type _ := (IteratedPolynomial 0).1
+def iterOne : Type _ := (IteratedPolynomial 1).1
 
--- end
+
+@[simp]
+theorem iterZero_is_nat : iterZero = ℕ := rfl
+
+@[simp]
+theorem iterOne_is_poly : iterOne = Polynomial ℕ := rfl
+
+-- theorem iterPoly_is_mvPoly (n : ℕ) : (IteratedPolynomial n).1 =
+
+end
 
 -- -- Define an Instance of the Property for Natural Numbers
 -- instance nat_property : Property Nat := { prop := fun n => n % 2 = 0 }
