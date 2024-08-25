f637b5f36e7e20314dd86d164cdc708c8f470019
Quang Dao <quangdao@umich.edu>
2024-08-25 17:38:40 -0600

    rename folder to `JoltConstraint`

---
+++ ZKLib/JoltConstraint/Bytecode.lean
@@ -0,0 +1,49 @@
+import ZKLib.JoltConstraint.Constants
+import ZKLib.JoltConstraint.Field
+import ZKLib.JoltConstraint.Trace
+
+/-!
+  # The bytecode checking of Jolt
+
+  This describes the read-only memory checking part for the bytecode in Jolt. Essentially, `Bytecode.Preprocessing` contains the information needed to check the bytecode witness.
+-/
+
+
+namespace Jolt
+
+variable (F : Type) [JoltField F]
+
+namespace Bytecode
+
+-- TODO: derive `Repr` for `HashMap`?
+-- TODO: replace `HashMap` with just `AssocList`? We don't care too much about performance here
+-- We can prove that the keys are distinct, assuming we do preprocessing on a valid `ELF` file
+structure Preprocessing where
+  codeSize : UInt64
+  vInitFinal : Fin NUM_BYTECODE_VALUE_FIELDS → Array F
+  virtualAddressMap : Array (UInt64 × UInt64)
+deriving Repr, Inhabited, DecidableEq
+
+
+-- Generate preprocessing data from `Array ELFInstruction` and `Array (UInt64 × UInt8)`
+
+def Preprocessing.new (bytecode : Array Row) : Preprocessing F := sorry
+-- Id.run do
+--   assert! bytecode.size > 0
+--   let codeSize := bytecode.size
+--   let vInitFinal := Array.range NUM_BYTECODE_VALUE_FIELDS |> Array.map (fun _ => Array.range codeSize |> Array.map (fun _ => 0 : F))
+--   let virtualAddressMap := HashMap.empty
+--   { codeSize := codeSize, vInitFinal := vInitFinal, virtualAddressMap := virtualAddressMap }
+
+structure Witness where
+  readWriteAddress : Array F
+  readWriteValue : Fin NUM_BYTECODE_VALUE_FIELDS → Array F
+  readTimestamp : Array F
+  finalBytecodeTimestamp : Array F
+deriving Repr, Inhabited, DecidableEq
+
+def isValid (preprocessing : Preprocessing F) (wit : Witness F) : Prop := sorry
+
+end Bytecode
+
+end Jolt
---
+++ ZKLib/ToMathlib/UInt/Equiv.lean
@@ -0,0 +1,33 @@
+import Mathlib.Algebra.BigOperators.Fin
+
+#check finFunctionFinEquiv
+
+def finUInt8Equiv : Fin (2 ^ 8) ≃ UInt8 where
+  toFun := fun i => UInt8.mk i
+  invFun := fun u => u.val
+  left_inv := fun i => by simp
+  right_inv := fun u => by simp
+
+def finUInt16Equiv : Fin (2 ^ 16) ≃ UInt16 where
+  toFun := fun i => UInt16.mk i
+  invFun := fun u => u.val
+  left_inv := fun i => by simp
+  right_inv := fun u => by simp
+
+def finUInt32Equiv : Fin (2 ^ 32) ≃ UInt32 where
+  toFun := fun i => UInt32.mk i
+  invFun := fun u => u.val
+  left_inv := fun i => by simp
+  right_inv := fun u => by simp
+
+def finUInt64Equiv : Fin (2 ^ 64) ≃ UInt64 where
+  toFun := fun i => UInt64.mk i
+  invFun := fun u => u.val
+  left_inv := fun i => by simp
+  right_inv := fun u => by simp
+
+def finBitVecEquiv : Fin (2 ^ n) ≃ BitVec n where
+  toFun := fun i => BitVec.ofFin i
+  invFun := fun u => u.toFin
+  left_inv := fun i => by simp
+  right_inv := fun u => by simp
